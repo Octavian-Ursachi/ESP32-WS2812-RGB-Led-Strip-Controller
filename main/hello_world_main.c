4ded0fd04abcd211fd001e547b592df89d823e24
octavian <ursachioctavian2@gmail.com>
2024-12-15 15:07:52 +0200

    Initial commit

---
+++ main/hello_world_main.c
@@ -0,0 +1,24 @@
+/*
+ * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_wifi.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+
+void app_main(void)
+{
+
+    while(1){
+        printf("Hello world!\n");
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
